4d4e0374fb03cba736978e5a5e71a73860290fdb
Helena <inger.helena@gmail.com>
2020-07-26 12:57:24 +0100

    refactored owner and siteLocation variables

--- js/app.js
+++ js/app.js
@@ -12,23 +12,27 @@ document.addEventListener('DOMContentLoaded', () => {
         return event.target.name.value;
     }
 
+    const siteLocation = function (event) {
+        return event.target.location.value;
+    }
+
+    const owner = function (event) {
+        return event.target.owner.value;
+    }
+
 const handleFormSubmit = function (event) {
     event.preventDefault();
-
-    // const name = event.target.name.value;
-    const location = event.target.location.value;
-    const owner = event.target.owner.value;
 
     const nameDiv = document.createElement('div');
     nameDiv.textContent = `${name(event)}`;
     nameDiv.classList.add('name-item');
 
     const locationDiv = document.createElement('div');
-    locationDiv.textContent = `${location}`;
+    locationDiv.textContent = `${siteLocation(event)}`;
     locationDiv.classList.add('location-item');
 
     const ownerDiv = document.createElement('div');
-    ownerDiv.textContent = `${owner}`;
+    ownerDiv.textContent = `${owner(event)}`;
     ownerDiv.classList.add('owner-item');
 
     const listElement = document.createElement('li');
